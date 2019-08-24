1bf59d8f898e7915b83445d0fa238c1214075763
chills <contagnas@gmail.com>
2019-08-23 22:18:06 -0500

    Parse secrets from command line

--- src/main/scala/twitterstats/Main.scala
+++ src/main/scala/twitterstats/Main.scala
@@ -14,43 +14,70 @@ import org.http4s.client.oauth1.{Consumer, Token}
 import org.http4s.implicits._
 import org.typelevel.jawn.RawFacade
 
+import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.global
 
 private object Main extends IOApp {
-  val apiKey = "secret"
-  val apiSecretKey = "secret"
-  val accessToken = "secret"
-  val accessTokenSecret = "secret"
+  case class Config(
+    token: Token,
+    consumer: Consumer,
+    statusUri: Uri
+  )
 
-  val consumer = oauth1.Consumer(apiKey, apiSecretKey)
-  val token = oauth1.Token(accessToken, accessTokenSecret)
+  def parseConfig(args: List[String]): Config = {
+    case class OConfig(
+      apiKey: Option[String] = None,
+      apiSecretKey: Option[String] = None,
+      accessToken: Option[String] = None,
+      accessTokenSecret: Option[String] = None,
+      statusUri: Uri = uri"""https://stream.twitter.com/1.1/statuses/sample.json"""
+    ) {
+      def toConfig = Config(
+        consumer = Consumer(apiKey.get, apiSecretKey.get),
+        token = Token(accessToken.get, accessTokenSecret.get),
+        statusUri = statusUri
+      )
+    }
+
+    @tailrec
+    def parse(args: List[String], config: OConfig): OConfig =
+      args match {
+        case Nil => config
+        case "--apiKey" :: apiKey :: rest => parse(rest, config.copy(apiKey = Some(apiKey)))
+        case "--apiSecretKey" :: apiSecretKey :: rest => parse(rest, config.copy(apiSecretKey = Some(apiSecretKey)))
+        case "--accessToken" :: accessToken :: rest => parse(rest, config.copy(accessToken = Some(accessToken)))
+        case "--accessTokenSecret" :: accessTokenSecret :: rest => parse(rest, config.copy(accessTokenSecret = Some(accessTokenSecret)))
+        case "--statusUri" :: statusUri :: rest => parse(rest, config.copy(statusUri = Uri.unsafeFromString(statusUri)))
+        case _ => throw new IllegalArgumentException("Bad arguments")
+      }
+
+    parse(args, OConfig()).toConfig
+  }
 
   implicit val circeSupportParser: RawFacade[Json] = new CirceSupportParser(None, false).facade
 
-  def signRequest(consumer: Consumer, token: Token)(req: Request[IO]): IO[Request[IO]] = {
-    oauth1.signRequest(req, consumer, callback = None, verifier = None, token = Some(token))
-  }
-
-  val request: Request[IO] = Request(uri = uri"""https://stream.twitter.com/1.1/statuses/sample.json""")
-  val oauthRequest: IO[Request[IO]] = oauth1.signRequest(
-    req = request,
-    consumer = consumer,
-    callback = None,
-    verifier = None,
-    token = Some(token)
-  )
-
   def run(args: List[String]): IO[ExitCode] = {
-    val tweets: Stream[IO, Json] = for {
+    val config = parseConfig(args)
+
+    val request = oauth1.signRequest[IO](
+      req = Request(uri = config.statusUri),
+      consumer = config.consumer,
+      callback = None,
+      verifier = None,
+      token = Some(config.token)
+    )
+
+    val tweets: Stream[IO, String] = for {
       client <- BlazeClientBuilder[IO](global).stream
-      request <- Stream.eval(oauthRequest)
-      tweets <- client.stream(request).flatMap(_.body.chunks.parseJsonStream)
-    } yield tweets
+      request <- Stream.eval(request)
+      rawTweet <- client.stream(request)
+      tweet <- rawTweet.body.chunks.parseJsonStream
+      prettyTweet = tweet.spaces4
+    } yield prettyTweet
 
     val printer: Stream[IO, Unit] = Stream.resource(Blocker[IO]).flatMap {
       blocker =>
         tweets
-          .map(_.spaces2)
           .through(text.utf8Encode)
           .through(stdout(blocker))
     }
@@ -58,4 +85,3 @@ private object Main extends IOApp {
     printer.compile.drain.as(ExitCode.Success)
   }
 }
-
